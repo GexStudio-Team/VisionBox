0a27bb94f0f2c650a1ef02b3a3ac1987c23baa46
GexStudio <gexstudioteam@gmail.com>
2026-07-14 00:02:36 -0500

    feat: add health endpoint and Swagger documentation

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,15 +3,43 @@ from fastapi import FastAPI
 from app.core.config import settings
 
 app = FastAPI(
-    tittle=settings.PROJECT_NAME,
-    version=settings.VERSION
+    title=settings.PROJECT_NAME,
+    version=settings.VERSION,
+    description="""
+    VisionBox API
+
+    Backend oficial del sistema VisionBox.
+    Funciones Principales:
+
+    - Gestion de inventario
+    - Comunicacion con hardware
+    - Porcesamiento de IA
+    - Administacion del sistema
+
+    """
 )
 
-
-@app.get("/")
+@app.get(
+    "/",
+    tags=["System"],
+    summary="Root endpoint",
+    description="Returns the welcome message of the VisionBox API."
+)
 def root():
-    return{
-        "project": settings.PROJECT_NAME,
-        "version": settings.VERSION,
-        "status": "Backend Running"
+    return {
+        "message": "Welcome to VisionBox API"
+    }
+
+
+@app.get( "/health",
+    tags=["System"],
+    summary="Health Check",
+    description="Returns the current status of the VisionBox backend",
+)
+
+def health():
+    return {
+        "status": "online",
+        "service": settings.PROJECT_NAME,
+        "version": settings.VERSION
     }
